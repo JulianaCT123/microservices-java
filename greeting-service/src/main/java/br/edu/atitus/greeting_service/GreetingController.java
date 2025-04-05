9756d044736b4508f6a43a4b5d9cd3480c1a5710
Juliana Castanho Teixeira <julianact2004@gmail.com>
2025-04-04 22:00:12 -0300

    Segunda Parte

--- greeting-service/src/main/java/br/edu/atitus/greeting_service/GreetingController.java
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/GreetingController.java
@@ -1,15 +1,30 @@
 package br.edu.atitus.greeting_service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting-service")
 public class GreetingController {
+	
+	@Value("${greeting-service.greeting}")
+	private String greeting;
+	@Value("${greeting-service.default-name}")
+	private String defaultName;
 
-	@GetMapping
-	public String getGreeting() {
-		return "Hello World!";
+	@GetMapping({"","/","/{namePath}"})
+	public ResponseEntity<String> getGreeting(
+			@PathVariable(required = false) String namePath,
+			@RequestParam(required = false) String name
+			) {
+		if (name == null)
+			name = namePath != null ? namePath : defaultName;
+		String returnText = String.format("%s %s!", greeting, name);
+		return ResponseEntity.ok(returnText);
 	}
 }
